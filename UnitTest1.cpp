9dead9a1cef0980f2ccc23051e6b1375f9581695
VadimZhary <vadim.zhary@gmail.com>
2022-11-27 18:27:50 +0200

    Init application

---
+++ UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\1\source\repos\ИПЗ лаба 5\ИПЗ лаба 5\ИПЗ лаба 5.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1 {
+	TEST_CLASS(calculate_Tests) {
+public:
+	TEST_METHOD(calculate_get0point5and2_2returned) {
+		int n = 2;
+		float x = 0.5;
+		float expected = 2;
+
+		float actual = calculate(x, n);
+
+		Assert::AreEqual(expected, actual);
+	}
+public:
+	TEST_METHOD(calculate_get1and1_0returned) {
+		int n = 1;
+		float x = 1;
+		float expected = 0;
+
+		float actual = calculate(x, n);
+
+		Assert::AreEqual(expected, actual);
+	}
+	};
+}
---
+++ ИПЗ лаба 5.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <math.h>
+#include <ctime>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+// Checking the entered data
+void checkValidParams(double start, double end) {
+	if (start > end) {
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //If there is an error, the text will be red
+		SetConsoleTextAttribute(handle, 4);
+		throw "Incorrect data!!!";
+	}
+}
+
+void checkValidParams(double start, double end, double step) {
+	if (step <= 0) {
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //If there is an error, the text will be red
+		SetConsoleTextAttribute(handle, 4);
+		throw "Incorrect data!!!";
+	}
+}
+
+//Знаходження функції
+double calculate(double x, int n) {
+	if (x > 0) {
+		double y = 1;
+		if (n < 0) {
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //If there is an error, the text will be red
+			SetConsoleTextAttribute(handle, 4);
+			cout << "Error!!!";
+			exit(0);
+		}
+		for (int i = 1; i <= n; i++) {
+			y *= (1 / x) - (1 / i);
+		}
+		return y;
+	}
+	else {
+		double y = 0;
+		if (n - 1 < 0) {
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //If there is an error, the text will be red
+			SetConsoleTextAttribute(handle, 4);
+			cout << "Error!!!";
+			exit(0);
+		}
+		else {
+			for (int i = 0; i <= n - 1; i++) {
+				for (int j = 0; j <= n - 1; j++) {
+					y += 1 / (x - i + j);
+				}
+			}
+			return y;
+		}
+	}
+}
+
+//Recording to disk answers
+void printValues(double start, double end, double step, int n, int store) {
+	ofstream fout("result.txt");
+	for (double x = start; x <= end; x += step) {
+		double y = calculate(x, n);
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); //If the value is correct, the text will be green
+		SetConsoleTextAttribute(handle, 2);
+		cout << "x = " << x << "\ny = " << y << endl;
+		if (store == 1) {
+			fout << "x= " << x << endl;
+			fout << "y= " << y << endl;
+		}
+	}
+	if (store == 1) {
+		system("notepad C:\\Users\\1\\source\\repos\\ИПЗ лаба 5\\ИПЗ лаба 5\\result.txt"); //Automatically opens a text file with a record of the answer
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Ukr");
+	float start, end, step;
+	int n, store;
+	try {
+		ifstream infile("number.txt");
+		//reading from a file
+		infile >> start;
+
+		infile >> end;
+		checkValidParams(start, end);
+
+		infile >> step;
+		checkValidParams(start, end, step);
+
+		infile >> n;
+
+		cout << "If you want to save the function value, enter 1, otherwise 0: \n";
+		cin >> store;
+		printValues(start, end, step, n, store);
+	}
+	catch (const char* ex) {
+		cout << ex << endl;
+		return -1;
+	}
+	catch (...) {
+		cout << "Unknown error!!!" << endl;
+		return -2;
+	}
+}
